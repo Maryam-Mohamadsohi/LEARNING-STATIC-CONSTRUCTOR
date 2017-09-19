bd957de914255cffcc3bba8bad2bf73b48264795
Maryam-Mohamadsohi <m2sohi@outlook.com>
2017-09-19 15:57:27 +0430

    Add project files.

---
+++ LEARNING-STATIC-CONSTRUCTOR/Bus.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LEARNING_STATIC_CONSTRUCTOR
+{
+    public class Bus
+    {
+        // Static variable used by all Bus instances.
+        // Represents the time the first bus of the day starts its route.
+        protected static readonly System.DateTime globalStartTime;
+
+        // Property for the number of each bus.
+        protected int RouteNumber { get; set; }
+
+        // Static constructor to initialize the static variable.
+        // It is invoked before the first instance constructor is run.
+        static Bus()
+        {
+            globalStartTime = System.DateTime.Now;
+
+            // The following statement produces the first line of output, 
+            // and the line occurs only once.
+            System.Console.WriteLine("Static constructor sets global start time to {0}",
+                globalStartTime.ToLongTimeString());
+        }
+
+        // Instance constructor.
+        public Bus(int routeNum)
+        {
+            RouteNumber = routeNum;
+            System.Console.WriteLine("Bus #{0} is created.", RouteNumber);
+        }
+
+        // Instance method.
+        public void Drive()
+        {
+            System.TimeSpan elapsedTime = System.DateTime.Now - globalStartTime;
+
+            // For demonstration purposes we treat milliseconds as minutes to simulate
+            // actual bus times. Do not do this in your actual bus schedule program!
+            System.Console.WriteLine("{0} is starting its route {1:N2} minutes after global start time {2}.",
+                                    this.RouteNumber,
+                                    elapsedTime.TotalMilliseconds,
+                                    globalStartTime.ToShortTimeString());
+        }
+    }
+
+}
---
+++ LEARNING-STATIC-CONSTRUCTOR/TestBus.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace LEARNING_STATIC_CONSTRUCTOR
+{
+    class TestBus
+    {
+        static void Main()
+        {
+            // The creation of this instance activates the static constructor.
+            Bus bus1 = new Bus(71);
+
+            // Create a second bus.
+            Bus bus2 = new Bus(72);
+
+            // Send bus1 on its way.
+            bus1.Drive();
+
+            // Wait for bus2 to warm up.
+            System.Threading.Thread.Sleep(25);
+
+            // Send bus2 on its way.
+            bus2.Drive();
+
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+
+    // In this example, class Bus has a static constructor.
+    //When the first instance of Bus is created(bus1),
+    //the static constructor is invoked to initialize the class.
+    //The sample output verifies that the static constructor runs only one time,
+    //even though two instances of Bus are created,
+    //and that it runs before the instance constructor runs.
+
+    /* Sample output:
+        Static constructor sets global start time to 3:57:08 PM.
+        Bus #71 is created.
+        Bus #72 is created.
+        71 is starting its route 6.00 minutes after global start time 3:57 PM.
+        72 is starting its route 31.00 minutes after global start time 3:57 PM.      
+   */
+}
